ad96cd129a1f3a8b0e1678ab25d15164edea4dd6
Parth Kasmalkar <parth.kasmalkar@warriorlife.net>
2018-01-23 14:23:12 -0800

    Moved Score adding to HTML

--- webapp.js
+++ webapp.js
@@ -1,21 +1,5 @@
 // JavaScript Document
 
-// Define Score Variable
-var currentScore = 0;
-
-// Add Score Functions
-function addScore1() {
-	currentScore++;
-	document.getElementById("score").value = currentScore;
-}
-	
-	
-/*
-Add 5 Function
-function addScore5() {
-	currentScore = currentScore + 5
-}
-*/
 
 // Random Function
 function start() {
